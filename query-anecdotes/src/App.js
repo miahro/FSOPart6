503f08e4b8dc083023474e0ccd1e30917ca4e129
miahro <mikko.ahro@gmail.com>
2023-05-28 17:37:43 +0300

    Ex 6.20-6.22

---
+++ query-anecdotes/src/App.js
@@ -0,0 +1,60 @@
+import { useMutation, useQuery, useQueryClient } from 'react-query'
+import { getAnecdotes, updateAnecdote } from './requests'
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+
+const App = () => {
+
+  const queryClient = useQueryClient()
+
+  const updateAnecdoteMutation = useMutation(updateAnecdote, {
+    onSuccess: (updatedAnecdote) => {
+      queryClient.invalidateQueries('anecdotes')
+    }
+  })
+
+  const handleVote = (anecdote) => {
+    console.log('vote', anecdote)
+    const id = anecdote.id
+    console.log(id)
+    updateAnecdoteMutation.mutate({...anecdote})
+    
+  }
+
+  const result = useQuery('anecdotes', getAnecdotes, {
+    retry: 1,
+    refetchOnWindowFocus: false
+  })
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  } else if ( result.isError) {
+    return <div>anecdote service not available dut to problems in server</div>
+  }
+
+  const anecdotes = result.data
+
+  return (
+    <div>
+      <h3>Anecdote app</h3>
+    
+      <Notification />
+      <AnecdoteForm />
+    
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => handleVote(anecdote)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
---
+++ query-anecdotes/src/requests.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAnecdotes = () =>
+  axios.get(baseUrl).then(res => res.data)
+
+export const createAnecdote = newNote =>
+  axios.post(baseUrl, newNote).then(res => res.data)
+
+export const updateAnecdote = updatedNote =>
+  axios.put(`${baseUrl}/${updatedNote.id}`, {...updatedNote, votes: updatedNote.votes+1}).then(res => res.data)
